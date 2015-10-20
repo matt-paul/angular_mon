3a577bf4d9fd6cd2ef772abbad1a5ad3b449f22d
Dom Batten <dominic.batten@googlemail.com>
2015-10-20 16:52:12 +0100

    added test to searchfactory spec

---
+++ test/searchfactory.spec.js
@@ -0,0 +1,32 @@
+describe('factory: Search', function() {
+
+  var search;
+
+  beforeEach(module('GitUserSearch'));
+
+  beforeEach(inject(function(Search) {
+    search = Search;
+  }));
+
+  beforeEach(inject(function($httpBackend) {
+    httpBackend = $httpBackend
+    httpBackend
+      .when("GET", "https://api.github.com/search/users?access_token="+ gitAccessToken + "&q=hello")
+      .respond(
+        { items: items }
+      );
+  }));
+
+  it('responds to query', function() {
+    expect(search.query).toBeDefined();
+  });
+
+  it('returns search results', function() {
+    search.query('hello')
+      .then(function(response) {
+        expect(response.data).toEqual(items)
+      })
+    httpBackend.flush();
+  })
+
+});
